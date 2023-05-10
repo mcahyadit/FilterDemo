145cf51bc9da1860cc50dae2d9d55d8a6c7c84a6
mcahyadit <milkeelke@gmail.com>
2023-05-10 21:06:43 +0800

    commit #5: fullscreen support for API>29

--- app/src/main/java/com/mosoft/filterdemo/MainActivity.kt
+++ app/src/main/java/com/mosoft/filterdemo/MainActivity.kt
@@ -4,9 +4,9 @@ import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.view.WindowInsets
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment
 import com.mosoft.filterdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,9 +17,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //FFMPEGDownloader()
-        //AndroidFFMPEGLocator(this)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -28,13 +25,18 @@ class MainActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
-        } else {  // 16...29
+        } else if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {  // 16...29
             window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
             supportActionBar?.hide()
             window.setFlags(
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
+        } else {
+            window.decorView.windowInsetsController?.hide(WindowInsets.Type.systemBars())
         }
+
+        //FFMPEGDownloader()
+        //AndroidFFMPEGLocator(this)
     }
 }
